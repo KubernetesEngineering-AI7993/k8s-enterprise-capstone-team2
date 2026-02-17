a51df330027b75b1bb15cfa158fec3651903587a
Job King <jobezking@gmail.com>
2026-02-17 18:07:37 -0500

    week03-04 deliverabkes

--- week03-04/lab03/lab03.sh
+++
@@ -1,9 +0,0 @@
-sudo kubectl get deployment metrics-server -n kube-system
-sudo kubectl get --raw "/apis/metrics.k8s.io/v1beta1/nodes"
-sudo kubectl apply -f sample-deployment.yaml
-sudo kubectl apply -f sample-service.yaml
-sudo kubectl apply -f hpa.yaml
-sudo kubectl get hpa
-sudo kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://sample-app; done"
-sudo kubectl get hpa sample-hpa --watch
-sudo kubectl get pods -l app=sample-app --watch
